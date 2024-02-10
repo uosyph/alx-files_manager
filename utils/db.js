5fb14ba738b2a7c32f9a1279f0fcb0fc6e15c110
Yousef Saeed <yousafesaeed@gmail.com>
2024-02-10 15:43:20 +0200

    Return Collections

--- utils/db.js
+++ utils/db.js
@@ -24,13 +24,19 @@ class DBClient {
   }
 
   async nbUsers() {
-    const userCount = this.users.countDocuments();
-    return userCount;
+    return this.users.countDocuments();
   }
 
   async nbFiles() {
-    const fileCount = this.files.countDocuments();
-    return fileCount;
+    return this.files.countDocuments();
+  }
+
+  async usersCollection() {
+    return this.users;
+  }
+
+  async filesCollection() {
+    return this.files;
   }
 }
 
